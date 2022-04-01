e469edbc7d095f2691a37f8da16dae5ea6162632
bettercallkyaw <bettercallkyaw@outlook.com>
2022-04-01 07:29:13 +0630

    django poll app

---
+++ poll_favorite_app/urls.py
@@ -0,0 +1,36 @@
+"""poll_favorite_app URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from django.http import HttpResponse
+
+# def home(request):
+#     return HttpResponse('<h1>Home Page</h1>')
+
+# def about(request):
+#     return HttpResponse('<h1>About Page</h1>')
+
+# def service(request):
+#     return HttpResponse('<h1>Service Page</h1>')
+
+urlpatterns = [
+    path('',include('pages.urls')),
+    path('polls/',include('polls.urls')),
+    path('admin/', admin.site.urls),
+    # path('',home),
+    # path('about/',about),
+    # path('service/',service)
+]
---
+++ polls/views.py
@@ -0,0 +1,43 @@
+from django.template import loader
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from .models import Question,Choice
+from django.urls import reverse
+
+#get question and display them
+def index(request):
+    latest_question_list=Question.objects.order_by('-pub_date')[:5]
+    context={'latest_question_list':latest_question_list}
+    return render(request,'polls/index.html',context)
+
+#show specific question and choices
+def detail(request,question_id):
+    try:
+        question=Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    return render(request,'polls/detail.html',{'question':question}) 
+
+#get question and display results
+def results(request,question_id):
+    question=get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{'question':question})
+
+#vote for question choice
+def vote(request,question_id):
+    question=get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice=question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        # redisplay the question voting form
+        return render(request,'polls/detail.html',{
+            'question':question,
+            'error_message':"you did't select a choice"
+        }) 
+    else:
+        selected_choice.votes+=1
+        selected_choice.save()  
+        # Always return an HttpReponseRedirect after successfully dealing
+        # with POST data.This prevents data from being posted twice if a
+        # use hits the back button 
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))         
